b470261525957ff8d0bef9c2b8aa76b2b96ef92b
victorronneling <victoronneling@gmail.com>
2024-09-29 07:59:59 +0200

    test1

---
+++ Main.py
@@ -0,0 +1,11 @@
+import time
+import psutil
+
+while True:
+    print(psutil.cpu_times_percent())
+    print()
+    print(psutil.virtual_memory().percent)
+    time.sleep(1)
+    print(psutil.cpu_percent())
+
+
